4c56c929cdcde9ffc5512fd2ceeb502189af5b48
JohanJAC1988 <johanjacm@gmail.com>
2025-07-10 13:11:43 -0500

    Update store.js
    Actualización de Store.

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+import cartReducer from './CartSlice'; // Aquí el nombre es más corto y convencional
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer, // Usa el nombre de la variable importada
+  },
 });
-export default store
+
+export default store;
